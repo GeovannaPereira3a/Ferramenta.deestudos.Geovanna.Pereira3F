3e543ae6a42f0a2b9c987cdec8ff96dba6212a56
GeovannaPereira3a <00001082880723sp@al.educacao.sp.gov.br>
2024-11-21 10:48:47 -0300

    Add files via upload

---
+++ Ferramenta de estudos - Geovanna Pereira/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é html?',
+    'O html é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Estados Unidos?',
+    'A capital dos Estados Unidos é Washington, D.C.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função afim?',
+    'A função afim é qualquer função que sua formação seja y = ax + b, sendo a e b números reais e a diferente de zero.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz hoje em Inglês?',
+    'Hoje em ingles é TODAY (TCHUDEI)'
+)
